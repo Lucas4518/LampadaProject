226e47e0849f2c011d4a001e89bf45bee9dd6cef
Lucas4518 <arthurandradedasilva84@gmail.com>
2025-03-11 21:24:13 -0300

    Part1

---
+++ assets/js/js.js
@@ -0,0 +1,6 @@
+function ligarLampada() {
+    document.getElementById("lampada").src = "assets/img/Lampada_on-removebg-preview.png";
+}
+function desligarLampada() {
+    document.getElementById("lampada").src = "assets/img/pngtree-light-bulb-turn-off.png";
+}
